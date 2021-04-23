4f4f0be8d96b0220161007c54825a546614ae546
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2021-04-23 07:16:47 +0200

    Removed sqlsrv module from tests.

--- 7/tests/run.sh
+++ 7/tests/run.sh
@@ -6,7 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-git_url=https://github.com/robertoperuzzo/php-sqlsrv.git
+git_url=https://github.com/robertoperuzzo/php-as400.git
 
 wait_for_cron() {
     executed=0
